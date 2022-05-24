a8711ba6f02704380a723530dc47df355df265ed
Andhikawicaksono <105833482+Andhikawicaksono@users.noreply.github.com>
2022-05-24 09:36:56 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalkan </h1>";
+        echo "nama saya andhika wicaksonooooo 
+        olahraga yang saya sukaii adalah badminton";
+    }
+
+}
